0d7f3b9cd78490d2cc6b7297b1fec67205aed09b
Otabek <ojuraev@gmail.com>
2021-06-06 11:35:32 +0900

    get user data

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import requests
 import json
 from pprint import pprint
 from os import times
+
+def get_user(data):
+    user = {} 
+    user['gender']= data["results"][0]["gender"] 
+    user['first'] = data["results"][0]["name"]["first"]
+    user['last'] = data["results"][0]["name"]["last"]
+    user['city'] = data["results"][0]['location']['city']
+    return user
