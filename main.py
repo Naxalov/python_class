5bd93b39b0789edecf7718a8f776f0989c2da660
Otabek <ojuraev@gmail.com>
2021-06-06 11:36:34 +0900

    save user data into list

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def get_user(data):
     user['last'] = data["results"][0]["name"]["last"]
     user['city'] = data["results"][0]['location']['city']
     return user
+
+def save_user(users):
+    users_list = {
+    'results':users
+    }
+    f = open(f'data_point/{times}_randomuser.json','w')
+    results = json.dumps(users_list)
+    f.write(results)
+    return f.close()
